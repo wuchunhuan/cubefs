703397b932d8780d1611fe4fbe374185265ebf4c
lixiang11 <lixiang11@oppo.com>
2021-05-27 05:27:49 -0700

    content-summary with xattr

--- metanode/partition.go
+++ metanode/partition.go
@@ -127,6 +127,7 @@ type OpExtend interface {
 	BatchGetXAttr(req *proto.BatchGetXAttrRequest, p *Packet) (err error)
 	RemoveXAttr(req *proto.RemoveXAttrRequest, p *Packet) (err error)
 	ListXAttr(req *proto.ListXAttrRequest, p *Packet) (err error)
+	UpdateXAttr(req *proto.UpdateXAttrRequest, p *Packet) (err error)
 }
 
 // OpDentry defines the interface for the dentry operations.
@@ -136,6 +137,7 @@ type OpDentry interface {
 	DeleteDentryBatch(req *BatchDeleteDentryReq, p *Packet) (err error)
 	UpdateDentry(req *UpdateDentryReq, p *Packet) (err error)
 	ReadDir(req *ReadDirReq, p *Packet) (err error)
+	ReadDirOnly(req *ReadDirOnlyReq, p *Packet) (err error)
 	Lookup(req *LookupReq, p *Packet) (err error)
 	GetDentryTree() *BTree
 }
