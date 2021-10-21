7d0a734fd70b6255f634e61eeac7227a81b2d866
leonrayang <chl696@sina.com>
2021-10-21 10:14:20 +0800

    add log.

--- metanode/manager_op.go
+++ metanode/manager_op.go
@@ -226,6 +226,7 @@ func (m *metadataManager) opCreateDentry(conn net.Conn, p *Packet,
 	}
 	err = mp.CreateDentry(req, p)
 	m.respondToClient(conn, p)
+
 	log.LogDebugf("%s [opCreateDentry] req: %d - %v, resp: %v, body: %s",
 		remoteAddr, p.GetReqID(), req, p.GetResultMsg(), p.Data)
 	return
