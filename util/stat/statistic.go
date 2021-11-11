9b3d4e57c52b500306a06909151931d51de8d896
lixiang11 <lixiang11@oppo.com>
2021-11-11 09:50:18 +0800

    defer stat() --> defer func() { stat() }()

--- util/stat/statistic.go
+++ util/stat/statistic.go
@@ -198,7 +198,7 @@ func EndStat(typeName string, err error, bgTime *time.Time, statCount uint32) er
 		}
 	}
 
-	return AddStat(typeName, err, bgTime, statCount)
+	return addStat(typeName, err, bgTime, statCount)
 }
 
 func WriteStat() error {
@@ -284,7 +284,7 @@ func ClearStat() {
 	gSt.typeInfoMap = make(map[string]*typeInfo)
 }
 
-func AddStat(typeName string, err error, bgTime *time.Time, statCount uint32) error {
+func addStat(typeName string, err error, bgTime *time.Time, statCount uint32) error {
 	if gSt == nil {
 		return nil
 	}
