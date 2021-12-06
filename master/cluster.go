1d04aeed8f0b2cab442feb02a89826e91fc2022f
Victor1319 <834863182@qq.com>
2021-12-06 11:17:51 +0800

    enhance: when load mps&dps from rocksDB, add recovering dp&mp to badPartitionsIdMap again
    Signed-off-by: Victor1319 <834863182@qq.com>

--- master/cluster.go
+++ master/cluster.go
@@ -1398,7 +1398,7 @@ func (c *Cluster) validateDecommissionDataPartition(dp *DataPartition, offlineAd
 		return
 	}
 
-	if dp.isRecover {
+	if dp.isRecover && !dp.activeUsedSimilar() {
 		err = fmt.Errorf("vol[%v],data partition[%v] is recovering,[%v] can't be decommissioned", vol.Name, dp.PartitionID, offlineAddr)
 		return
 	}
@@ -1528,7 +1528,7 @@ func (c *Cluster) removeDataReplica(dp *DataPartition, addr string, validate boo
 	}
 
 	ok := c.isRecovering(dp, addr)
-	if ok {
+	if ok && !dp.activeUsedSimilar() {
 		err = fmt.Errorf("vol[%v],data partition[%v] can't decommision until it has recovered", dp.VolName, dp.PartitionID)
 		return
 	}
